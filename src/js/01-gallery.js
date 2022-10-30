54354f6e6b2fc0aa65d030234604e8e8372ff87d
Michał Murawski <m.a.murawski@wp.pl>
2022-10-30 23:11:51 +0100

    add code from homework 7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,39 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryParent = document.querySelector('.gallery');
+const markupGallery = addImageToMarkupGallery(galleryItems);
+const galleryHtml = addGalleryToHtml(markupGallery, galleryParent);
+galleryParent.addEventListener('click', resetDownloadFunction);
+const lightboxGallery = createNewSimpleLightbox('.gallery a');
+
+function addImageToMarkupGallery(imagesTable) {
+  return imagesTable
+    .map(
+      image =>
+        `<a class="gallery__item" href="${image.original}">
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.description}"
+            />
+        </a>`
+    )
+    .join('\r\n');
+}
+
+function addGalleryToHtml(gallery, htmlElement) {
+  htmlElement.innerHTML = gallery;
+}
+
+function resetDownloadFunction(event) {
+  event.preventDefault();
+}
+
+function createNewSimpleLightbox(className) {
+  return new SimpleLightbox(className, {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}

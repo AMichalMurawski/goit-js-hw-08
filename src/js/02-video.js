a2902e1c8c3dbc8e3932eda7dd1472c5fb02560d
Michał Murawski <m.a.murawski@wp.pl>
2022-10-31 23:24:28 +0100

    localStorage: 'videoplayer-current-time'

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', setCurrentTime);
+
+player
+  .setCurrentTime(getLocalStorage(PLAYER_CURRENT_TIME))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          `${error}`,
+          '\n',
+          `Seconds value: ${getLocalStorage(PLAYER_CURRENT_TIME)}`
+        );
+        break;
+
+      default:
+        console.log(`${error}`);
+        break;
+    }
+  });
+
+function setCurrentTime(data) {
+  setLocalStorage(PLAYER_CURRENT_TIME, data.seconds);
+}
+
+function setLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getLocalStorage(key) {
+  try {
+    return localStorage.getItem(key);
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
